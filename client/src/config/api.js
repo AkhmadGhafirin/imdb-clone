614a8b0ab3c78c2fef8f423744126bf1461125ac
AkhmadGhafirin <ahmadghafirin@gmail.com>
2023-06-17 21:50:16 +0700

    update base url in client to public

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:3000/public'
-export const BASE_URL = 'https://api-cuisines.akhmadghafirin.com'
+export const BASE_URL = 'https://api-cuisines.akhmadghafirin.com/public'
